a92df662aabe53a82f5ba8adca761b8fd6dd15ac
schobert.karine <schobert.karine@outlook.com>
2026-03-02 15:08:25 +0100

    feat: auth + front home + legales pages

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'front_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('front/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
---
+++ src/Controller/Front/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PagesController extends AbstractController
+{
+#[Route('/mentions-legales', name: 'front_mentions', methods: ['GET'])]
+public function mentions(): Response
+{
+    return $this->render('front/pages/mentions.html.twig');
+}
+
+#[Route('/cgv', name: 'front_cgv', methods: ['GET'])]
+public function cgv(): Response
+{
+    return $this->render('front/pages/cgv.html.twig');
+}
+
+#[Route('/confidentialite', name: 'front_privacy', methods: ['GET'])]
+public function privacy(): Response
+{
+    return $this->render('front/pages/privacy.html.twig');
+}
+
+}
